af57f43b98787b64ffc5ec701b973069353af1c2
Julen Merino <julencio99@gmail.com>
2024-02-29 00:35:21 +0100

    Java programa begiratzeko

--- Erronka2/src/Liga/LigaOna.java
+++ Erronka2/src/Liga/LigaOna.java
@@ -5,53 +5,69 @@ import java.io.FileWriter;
 import java.io.IOException;
 import java.util.Scanner;
 
+import javax.swing.JOptionPane;
+
 public class LigaOna {
-    public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        String[] taldeak = new String[8];  // 8 talde
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
 
-        // Taldeen izenak lortu
-        for (int i = 0; i < 8; i++) {
-            System.out.print((i + 1) + ". taldearen izena sartu: ");
-            taldeak[i] = scanner.nextLine();
+		// Fitxategiaren izenak
+        String fitxategiarenIzena = JOptionPane.showInputDialog(null, "Zer izenarekin gorde nahi ditzu paritdoak");
+        
+        if (!fitxategiarenIzena.endsWith(".txt")) {
+            fitxategiarenIzena += ".txt";
         }
 
-        // Fitxategiaren izenak
-        String partiduFitxIzena = "partiduak.txt";
-        String sqlInsertFitxIzena = "sql_inserts.txt";
+        // Talde Kopurua eskatu
+        int taldeKop = Integer.parseInt(JOptionPane.showInputDialog(null, "Sartu Talde kopurua"));
 
-        try (BufferedWriter partiduWriter = new BufferedWriter(new FileWriter(partiduFitxIzena));
-             BufferedWriter sqlInsertWriter = new BufferedWriter(new FileWriter(sqlInsertFitxIzena))) {
-
-            // Egutegia sortu
-            int jornadaKopurua = 7;  // 7 jardunaldi
-            int partiduakJardunaldian = taldeak.length / 2;
-
-            for (int jardunaldia = 1; jardunaldia <= jornadaKopurua; jardunaldia++) {
-                partiduWriter.write("\n" + jardunaldia + ". jardunaldia:\n");
-                for (int partido = 0; partido < partiduakJardunaldian; partido++) {
-                    int localTaldea = partido;
-                    int bisitatzaileTaldea = (taldeak.length - 1) - partido;
-                    partiduWriter.write(taldeak[localTaldea] + " vs " + taldeak[bisitatzaileTaldea] + "\n");
-
-                    // INSERT INTO sententzia sortu
-                    String sqlInsert = "INSERT INTO Partidoa (Kod, Lokala, Bisitaria, Pts_lokala, Pts_bisitaria, Data, Hasiera_ordua, Txapelketa_kod) " +
-                            "VALUES (NULL, '" + taldeak[localTaldea] + "', '" + taldeak[bisitatzaileTaldea] + "', NULL, NULL, NULL, NULL, NULL);\n";
-                    sqlInsertWriter.write(sqlInsert);
-                }
-
-                // Taldeak biratu hurrengo jardunaldiarentzako
-                String temp = taldeak[taldeak.length - 1];
-                for (int i = taldeak.length - 1; i > 1; i--) {
-                    taldeak[i] = taldeak[i - 1];
-                }
-                taldeak[1] = temp;
+        // Txapelketa Kodea eskatu
+        String[] txapelketaKodeak = {"1-Gizona", "2-Emakumea", "3-Sub-18", "4-Sub-16"};
+        String txapelketaKodString = (String) JOptionPane.showInputDialog(null, "Sartu txapelketa kodea", "Txapelketa Kodea", JOptionPane.QUESTION_MESSAGE, null, txapelketaKodeak, txapelketaKodeak[0]);
+        int txapelketaKod = 0;
+        for (int i = 0; i < txapelketaKodeak.length; i++) {
+            if (txapelketaKodString.equals(txapelketaKodeak[i])) {
+                txapelketaKod = i + 1;
+                break;
             }
-
-            System.out.println("Egutegia partiduen " + partiduFitxIzena + " fitxategian eta SQL insert-en " + sqlInsertFitxIzena + " fitxategian gorde da.");
-        } catch (IOException e) {
-            System.err.println("Errorea fitxategiak idazterakoan");
-            e.printStackTrace();
         }
-    }
+
+        // Taldeen array-a
+        String[] taldeak = new String[taldeKop];
+
+        // Taldeen Izenak eskatu
+        for (int i = 0; i < taldeKop; i++) {
+            taldeak[i] = JOptionPane.showInputDialog(null, (i + 1) + ". taldearen izena sartu");
+		}
+
+		try (BufferedWriter sqlInsertWriter = new BufferedWriter(new FileWriter(fitxategiarenIzena))) {
+
+			// Egutegia sortu
+			int jornadaKopurua = taldeKop -1;  
+			int partiduakJardunaldian = taldeak.length / 2;
+			for (int jardunaldia = 1; jardunaldia <= jornadaKopurua; jardunaldia++) {
+				for (int partido = 0; partido < partiduakJardunaldian; partido++) {
+					int lokalTaldea = partido;
+					int bisitatzaileTaldea = (taldeak.length - 1) - partido;
+
+					// INSERT INTO sententzia sortu
+					String sqlInsert = "INSERT INTO Partidoa (Lokala, Bisitaria, Pts_lokala, Pts_bisitaria, Data, Hasiera_ordua, Txapelketa_kod) " +
+							"VALUES ('" + taldeak[lokalTaldea] + "', '" + taldeak[bisitatzaileTaldea] + "', NULL, NULL, NULL, NULL, " + txapelketaKod + ");\n";
+					sqlInsertWriter.write(sqlInsert);
+				}
+
+				// Talde aldaketak hurrengo jaurdunaldirako
+				String temp = taldeak[taldeak.length - 1];
+				for (int i = taldeak.length - 1; i > 1; i--) {
+					taldeak[i] = taldeak[i - 1];
+				}
+				taldeak[1] = temp;
+			}
+
+			System.out.println("SQL insert-en " + fitxategiarenIzena + " fitxategian gorde da.");
+		} catch (IOException e) {
+			System.err.println("Errorea fitxategiak idazterakoan");
+			e.printStackTrace();
+		}
+	}
 }
