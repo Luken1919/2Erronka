89dcbfbe557ffd350b784908fea9ea8060b85ec7
JulenMerino <146726718+JulenMerino@users.noreply.github.com>
2024-04-12 16:06:53 +0200

    Programa berria

---
+++ Erronka2/src/Liga/SQLManager.java
@@ -0,0 +1,63 @@
+package Liga;
+
+
+import javax.swing.*;
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.io.IOException;
+
+public class SQLManager {
+    public void insertPartidoak(int taldeKop, String txapelketaKodString, JTextField[] txtTaldeak) {
+        String[] taldeak = new String[taldeKop];
+        for (int i = 0; i < taldeKop; i++) {
+            taldeak[i] = txtTaldeak[i].getText();
+            if (taldeak[i].isEmpty()) {
+                JOptionPane.showMessageDialog(null, "Mesedez, sartu izenak guztiak.");
+                return;
+            }
+        }
+
+        String fitxategiarenIzena = "Partidoak.txt";
+
+        try (BufferedWriter sqlInsertWriter = new BufferedWriter(new FileWriter(fitxategiarenIzena, true))) {
+            int jornadaKopurua = taldeKop - 1;
+            int partiduakJardunaldian = taldeak.length / 2;
+            for (int jardunaldia = 1; jardunaldia <= jornadaKopurua; jardunaldia++) {
+                for (int partido = 0; partido < partiduakJardunaldian; partido++) {
+                    int lokalTaldea = partido;
+                    int bisitatzaileTaldea = (taldeak.length - 1) - partido;
+
+                    String sqlInsert = "INSERT INTO Partidoa (Lokala, Bisitaria, Txapelketa_kod) " +
+                            "VALUES ('" + taldeak[lokalTaldea] + "', '" + taldeak[bisitatzaileTaldea] + "', " + getTxapelketaKod(txapelketaKodString) + ");\n";
+                    sqlInsertWriter.write(sqlInsert);
+                }
+
+                String temp = taldeak[taldeak.length - 1];
+                for (int i = taldeak.length - 1; i > 1; i--) {
+                    taldeak[i] = taldeak[i - 1];
+                }
+                taldeak[1] = temp;
+            }
+
+            JOptionPane.showMessageDialog(null, "SQL insert-ak " + fitxategiarenIzena + " fitxategian gorde da.");
+        } catch (IOException e) {
+            System.err.println("Errorea fitxategiak idazterakoan");
+            e.printStackTrace();
+        }
+    }
+
+    private int getTxapelketaKod(String txapelketaKodString) {
+        switch (txapelketaKodString) {
+            case "1-Gizona":
+                return 1;
+            case "2-Emakumea":
+                return 2;
+            case "3-Sub-18":
+                return 3;
+            case "4-Sub-16":
+                return 4;
+            default:
+                return 0;
+        }
+    }
+}
